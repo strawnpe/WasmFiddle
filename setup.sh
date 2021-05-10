b4ed868ddfb65a334b53ae41255340c1d81e0e66
Kayla Smith <50057385+smithkaylar@users.noreply.github.com>
2021-05-10 19:18:49 -0400

    update script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-# https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+
 if [ ! -d "/lib" ]
 then
     mkdir "lib"
 fi
 
-# if ! [ -x "$(command -v git)" ]
-# then
-#     mkdir 'something'
-# fi
 cd lib
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk

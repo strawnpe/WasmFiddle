e370a8d0ab817dba119556044b50d89d630b8fe1
Kayla Smith <50057385+smithkaylar@users.noreply.github.com>
2021-05-29 00:38:19 -0400

    update codeoutput

--- client/src/CodeOutput.js
+++ client/src/CodeOutput.js
@@ -1,74 +1,39 @@
 import React from "react";
-import {instantiateStreaming} from "assemblyscript/lib/loader";
 
 class CodeOutput extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {data: null};
-        //const [data, setData] = React.useState(compiledCode);
-        const data = null;
-        console.log(data);
-
-        this.handleChange = this.handleChange.bind(this);
+        this.state = { filename: props.filename || null, data: props.data || null };
+        this.changeOutput = this.changeOutput.bind(this);
     };
 
-    handleChange(event) {
-        console.log(event.target.value);
-        this.setState({fetchCode: event.target.value.fetchCode, filename: event.target.value.filename});
-        let shortName = this.filename;
-        if (this.filename.endsWith('.c') || this.filename.endsWith('.cpp') || this.filename.endsWith('.rs')) {
-            const idx = this.filename.lastIndexOf('.');
+    componentDidUpdate = async (prevProps) => {
+        if (prevProps.filename !== this.props.filename) {
+          await this.changeOutput(this.props.filename);
+        }
+      }
+
+    changeOutput = async (filename) => {
+        let shortName = filename;
+        if (filename.endsWith('.c') || filename.endsWith('.cpp') || filename.endsWith('.rs')) {
+            const idx = filename.lastIndexOf('.');
             shortName = shortName.substring(0, idx);
         }
-        if (this.fetchCode) {
-            try {
-                instantiateStreaming(
-                    fetch(`https://wasmfiddle-314721.wl.r.appspot.com/${shortName}.wasm`)
-                ).then(rawModule => Object.assign({}, rawModule, {
-                    main: input => {
-                        // Create the string in memory and get the pointer
-                        const pInput = rawModule.__retain(rawModule.__allocString(input));
-                
-                        // Call the WebAssembly function
-                        const pOutput = rawModule.scramble(pInput);
-                
-                        // Retrieve the result string
-                        const result = rawModule.__getString(pOutput);
-                
-                        // Free up memory
-                        rawModule.__release(pInput);
-                        rawModule.__release(pOutput);
-                        this.data = result;
-                    }
-                }));
-                // const compiledWasm = fetch(`https://wasmfiddle-314721.wl.r.appspot.com/${shortName}.wasm`, {
-                //     method: 'GET',
-                //     headers: {},
-                //     body: {}
-                // });
-                // console.log(compiledWasm);
-                // this.setState({fetchCode: true});
-            } catch (e) {
-                console.log(e);
-            } 
-        }
+        this.setState({ filename: shortName }); 
     }
-
-    compiledCode(data) {
-        console.log(data);
-    }
-
-    // React.useEffect(() => {
-    //     fetch("/api")
-    //         .then((res) => res.json())
-    //         .then((data) => setData(data.message));
-    // }, []);
     render() {
+        let wasm_url;
+        let iframeData;
+        if (this.state.filename) {
+            wasm_url = `http://localhost:3001/files/${this.state.filename}.html`
+            iframeData=`<iframe src="${wasm_url}" width="540" height="450"></iframe>`;
+        }
+        console.log(wasm_url);
         return (
             <div className="ui fluid card">
                 <div className="content">
                     <h2 className="ui teal header">Code Output</h2>
-                    <p>{this.data || "Loading..."}</p>
+                    {wasm_url ? <div dangerouslySetInnerHTML={{ __html: iframeData }}/> : <p>Loading...</p>}
                 </div>
             </div>
         );

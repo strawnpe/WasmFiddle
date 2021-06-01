4a0b58752f431ebc5d6b1ce5a31f5c12ed39f027
Peter Strawn <strawnpe@oregonstate.edu>
2021-06-01 16:58:12 -0700

    Create about page

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,27 @@ import React from "react";
 import "./App.css";
 import Header from './Header';
 import CodeInput from './CodeInput';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AboutPage from './AboutPage';
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
       <div>
         <div>
-          <Header />
+        <a href="/"><Header /></a>
         </div>
         <br />
-        <div>
-          <CodeInput />
-        </div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route path="/">
+              <CodeInput />
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </div>
   );
 }
